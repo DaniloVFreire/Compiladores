173995a7b3aae84c912ce2b7588f983bce9392f0
Danilo Freire <danilo.v.estudo@gmail.com>
2022-08-22 18:52:16 -0300

    Corrigindo o reconhecimento de cadeias indevidas

--- chainParentesesRecognition.cpp
+++ chainParentesesRecognition.cpp
@@ -6,46 +6,72 @@ int FS1(string entrada, int posic, int pilha);
 int FSIf(string entrada, int posic, int pilha);
 int FSc();
 
-int main(){
+int main()
+{
     string entrada;
     getline(cin, entrada, '\n');
-    if(FSIf(entrada, 0, 0)){
-        cout << "Cadeia aceita"<< endl;
+    if (FSIf(entrada, 0, 0))
+    {
+        cout << "Cadeia aceita" << endl;
     }
-    else{
-        cout << "Cadeia Incorreta"<< endl;
+    else
+    {
+        cout << "Cadeia não aceita" << endl;
     }
     return 0;
 }
 
-int FSIf(string entrada, int posic, int pilha){
-    if(entrada[posic] == ')'&& pilha <= 0){
-        return FSc();
+int FSIf(string entrada, int posic, int pilha)
+{
+
+    if (entrada.size() >= posic + 1)
+    {
+        cout << posic << ", " << entrada[posic] << ", " << pilha << endl;
+        if (entrada[posic] == ')' && pilha <= 0)
+        {
+            return FSc();
+        }
+        else if (entrada[posic] == ')')
+        {
+            return FSIf(entrada, posic + 1, pilha - 1);
+        }
+        else if (entrada[posic] == '(')
+        {
+            return FS1(entrada, posic + 1, pilha + 1);
+        }
     }
-    else if(entrada[posic] == ')'){
-        return FSIf(entrada, posic+1, pilha -1);
-    }
-    else if(entrada[posic] == '('){
-        return FS1(entrada, posic+1, pilha+1);
+    else if (pilha > 0)
+    {
+        return 0;
     }
     return 1;
 }
 
-int FS1(string entrada, int posic, int pilha){
-    if(entrada[posic] == '('){
-        pilha++;
-        return FSIf(entrada, posic+1, pilha+1);
-    }
-    else if(entrada[posic] == ' '){
-        return FSIf(entrada, posic+1, pilha);
-    }
-    else if(entrada[posic] == ')'){
-        return FSIf(entrada, posic+1, pilha-1);
+int FS1(string entrada, int posic, int pilha)
+{
+
+    if (entrada.size() >= posic + 1)
+    {
+        cout << posic << ", " << entrada[posic] << ", " << pilha << endl;
+        if (entrada[posic] == '(')
+        {
+            pilha++;
+            return FSIf(entrada, posic + 1, pilha + 1);
+        }
+        else if (entrada[posic] == ' ')
+        {
+            return FSIf(entrada, posic + 1, pilha);
+        }
+        else if (entrada[posic] == ')')
+        {
+            return FSIf(entrada, posic + 1, pilha - 1);
+        }
     }
     return 0;
 }
 
-int FSc(){
-    cout << "Erro na leitura, char ) adicionado incorretamente"<< endl;
+int FSc()
+{
+    cout << "Erro na leitura, char ) adicionado incorretamente" << endl;
     return 0;
 }
