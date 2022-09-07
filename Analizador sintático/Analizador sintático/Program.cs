b717b82498d83d7109fe264d3f1755ea6912b1f3
Danilo Freire <danilo.v.estudo@gmail.com>
2022-09-07 12:04:37 -0300

    Initializing the Syntactic Analyzer

---
+++ Analizador sintático/Analizador sintático/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Analizador_sintático
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SyntaticAnalizer algo = new SyntaticAnalizer();
+        }
+
+    }
+}
---
+++ Analizador sintático/Analizador sintático/SyntaticAnalizer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Analizador_sintático
+{
+    internal class SyntaticAnalizer
+    {
+
+        public SyntaticAnalizer()
+        {
+
+        }
+
+        public static bool FLetter(char c)
+        {
+            return (char.IsLetter(c) && char.IsLower(c));
+        }
+        public static bool FNumber(char c)
+        {
+            return (char.IsDigit(c));
+        }
+        public static string FEnemy(string s)
+        {
+            string pattern = "enemyN";
+            int i;
+            for (i = 0 ; i < 6; ++i)
+            {
+                if (s[i] != pattern[i])
+                {
+                    Console.WriteLine("Error, Invalid enemy token, expected 'enemyN'");
+                    return "Error";
+                }
+            }
+            if (!FNumber(s[i]))
+            {
+                Console.WriteLine("Error, Invalid number token, expected number after 'enemyN'");
+                return "Error";
+            }
+            if (!FLetter(s[i + 1]))
+            {
+                Console.WriteLine("Error, Invalid letter token, expected letter after number");
+                return "Error";
+            }
+
+            return "<" + s + "enemyN>";
+        }
+
+    }
+}
