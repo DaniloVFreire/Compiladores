41493c9728c75878e8804a887448fcba9469f0d7
Wagner Anthony <59626222+zadhart@users.noreply.github.com>
2022-10-25 21:55:18 -0300

    Added new functions in syntactic analyzer

--- Analizador sintático/Analizador sintático/SyntaticAnalizer.cs
+++ Analizador sintático/Analizador sintático/SyntaticAnalizer.cs
@@ -23,9 +23,151 @@ public class Syntatic
         FS();
         return "teste";
     }
+
+    private bool RecEnemy(List<Token> _token_list)
+    {
+        Token i = _token_list[0];
+
+        if(i.getType() == "enemy")
+        {
+            return true;
+        }
+
+        else
+        { 
+            return false;
+        }
+    }
+
+    private bool RecAlly(List<Token> _token_list)
+    {
+        Token i = _token_list[0];
+
+        if(i.getType() == "ally")
+        {
+            return true;
+        }
+
+        else
+        { 
+            return false;
+        }
+
+    }
+
+    private bool RecSeparator(List<Token> _token_list)
+    {
+        Token i = _token_list[0];
+
+        if(i.getType() == "separator")
+        {
+            return true;
+        }
+
+        else
+        { 
+            return false;
+        }
+
+    }
+
+    private bool RecDelimiter(List<Token> _token_list)
+    {
+        Token i = _token_list[0];
+
+        if(i.getType() == "delimiter")
+        {
+            return true;
+        }
+
+        else
+        { 
+            return false;
+        }
+
+    }
+
+    private bool RecAllyGroup(List<Token> _token_list)
+    {
+        Token i = this.token_list[0];
+
+        if(RecAlly(_token_list) == true)
+        {
+            this.token_list.RemoveAt(0);
+
+            if(RecSeparator(_token_list) == true)
+            {
+                this.token_list.RemoveAt(0);
+
+                return RecAllyGroup(_token_list);
+            }
+
+            else
+            {
+                return true;
+            }
+        }
+
+        else
+        { 
+            return false;
+        }
+
+    }
+
+    private bool RecSelf(List<Token> _token_list)
+    {
+        Token i = _token_list[0];
+
+        if(i.getType() == "self")
+        {
+            return true;
+        }
+
+        else
+        { 
+            return false;
+        }
+
+    }
+
+    private bool RecEndLine(List<Token> _token_list)
+    {
+        Token i = _token_list[0];
+
+        if(i.getType() == "endLine")
+        {
+            return true;
+        }
+
+        else
+        { 
+            return false;
+        }
+
+    }
+
     public bool FS()
     {
-        Console.WriteLine(this.token_list);
+
+        foreach (var t in this.token_list)
+        {
+            Console.WriteLine(t.getType());
+        }   
+
+        Console.WriteLine("_______________________________________________");
+
+        RecAllyGroup(this.token_list);
+
+        if(RecDelimiter(this.token_list) == true)
+        {
+            this.token_list.RemoveAt(0);
+        }
+
+        if(RecSelf(this.token_list) == true)
+        {
+            this.token_list.RemoveAt(0);
+        }
 
         foreach (var t in this.token_list)
         {
