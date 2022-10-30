e3dffd2f92ef73d4005663570f6f3c7e9cb09db8
Danilo Freire <danilo.v.estudo@gmail.com>
2022-10-30 19:48:03 -0300

    correção no .gitgnore e adição de rota dinâmica para arquivo write.txt

---
+++ Compilador/Analizador sintático/Compiler.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using static Lexical_analizer;
+using static Syntatic;
+
+public class Compiler
+{
+    private List<Token> token_list;
+    private bool verbose;
+    public Compiler()
+    {
+        this.token_list = new List<Token>();
+        this.verbose = false;
+    }
+    public Compiler(bool _verbose)
+    {
+        this.token_list = new List<Token>();
+        this.verbose = _verbose;
+    }
+    public int RunCompilation()
+    {
+        Lexical_analizer lexical_analizer_instance = new Lexical_analizer(verbose);
+        Syntatic syntatic_instance = new Syntatic();
+        string[] archive_lines;
+        int line_counter = 0;
+        string fileName = "WriteText.txt";
+        string path = Path.Combine(Environment.CurrentDirectory, @"Data\", fileName);
+        Console.WriteLine(path);
+        archive_lines = File.ReadAllLines(path);
+
+        foreach (string input_line in archive_lines)
+        {
+            Console.WriteLine(input_line);
+            Console.WriteLine(input_line.Length);
+            if (!String.IsNullOrEmpty(input_line))
+            {//Recebimento de uma string e envio pra processamento pelo analizador lexico
+                lexical_analizer_instance.RunLexicalAnalizer(input_line, line_counter);
+                line_counter++;
+            }
+            //if (verbose) Console.WriteLine("Fim da análize léxica");
+        }
+
+        this.token_list = lexical_analizer_instance.getTokenList();
+        if (verbose && token_list.Count > 0)
+        {//mostra a lista recebida do analizador léxico
+         //Console.WriteLine("Tokens recebidos no compilador");
+            foreach (var token in token_list)
+            {
+                Console.WriteLine(token);
+            }
+        }
+
+        Console.WriteLine("________________________________________________________");
+        syntatic_instance.RunSyntaticAnalizer(this.token_list);
+
+        Console.ReadLine();
+        return 0;
+    }
+
+}
---
+++ Compilador/Analizador sintático/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Compiler;
+
+namespace CompilerS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Compiler compiler_instance = new Compiler(true);
+            Console.WriteLine("algo");
+            compiler_instance.RunCompilation();
+        }
+    }
+    
+}
