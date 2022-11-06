6f5b6024236916b9a2b643dc039a6c5aa75e81c3
Danilo Freire <danilo.v.estudo@gmail.com>
2022-11-06 14:29:54 -0300

    Organizando diretórios e buscando arquivo WriteText.txt dinamicamente

---
+++ Compilador/Analizador sintático/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Compiler;
+
+namespace CompilerS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Compiler compiler_instance = new Compiler(true);
+            compiler_instance.RunCompilation();
+        }
+    }
+    
+}
